9cd4a08bccad3e1ccdbc915eb2a389a9232ecb1e
arne <arne@decsai.ugr.es>
2022-06-10 16:38:34 +0200

    change default path

--- src/wsi_to_patch_dataset.py
+++ src/wsi_to_patch_dataset.py
@@ -241,7 +241,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", "-dd", type=str, default="/data/BasesDeDatos/Panda")
+    parser.add_argument("--data_dir", "-dd", type=str, default="/data/BasesDeDatos/Panda/original")
     # parser.add_argument("--data_dir", "-dd", type=str, default="/home/arne/datasets/Panda")
     parser.add_argument("--wsi_dataframe", "-wd", type=str, default="./artifacts/train.csv")
     parser.add_argument("--wsi_list", "-wl", type=str, default="all")
